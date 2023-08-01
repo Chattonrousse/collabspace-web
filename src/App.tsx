6178cffeb64c082d47812896b786c82c8dfc8fb0
Geovanna Candido da Silva <gecandido13@gmail.com>
2023-07-31 22:47:49 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+
+import AppRoutes from "./routes";
+
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+
